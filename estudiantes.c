e5e704542a965522382ceecc3598473cf75cdc0f
Erick0321 <erick.velasquez@udla.edu.ec>
2026-04-15 22:15:29 -0500

    Se agrega visualización de tabla de calificaciones

--- estudiantes.c
+++ estudiantes.c
@@ -11,6 +11,9 @@ int main() {
 
     printf("Sistema de gestion de calificaciones\n");
 
+    ingresarCalificaciones(notas);
+    mostrarTabla(notas);
+
     return 0;
 }
 void ingresarCalificaciones(float notas[ESTUDIANTES][ASIGNATURAS]) {
@@ -20,11 +23,24 @@ void ingresarCalificaciones(float notas[ESTUDIANTES][ASIGNATURAS]) {
     for (i = 0; i < ESTUDIANTES; i++) {
         for (j = 0; j < ASIGNATURAS; j++) {
             do {
-                printf("Estudiante %d, Asignatura %d: ", i + 1, j + 1);
                 scanf("%f", &nota);
             } while (nota < 0 || nota > 10);
 
             notas[i][j] = nota;
         }
+    }
+}
+
+void mostrarTabla(float notas[ESTUDIANTES][ASIGNATURAS]) {
+    int i, j;
+
+    printf("TABLA DE CALIFICACIONES\n");
+
+    for (i = 0; i < ESTUDIANTES; i++) {
+        printf("Estudiante %d: ", i + 1);
+        for (j = 0; j < ASIGNATURAS; j++) {
+            printf("%.1f ", notas[i][j]);
+        }
+        printf("\n");
     }
 }
