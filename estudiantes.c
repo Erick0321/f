8c521c2566bf5e1503eccf86a7e90fb0656b9503
Erick0321 <erick.velasquez@udla.edu.ec>
2026-04-15 21:48:54 -0500

    Estructura inicial del programa y definición de constantes

---
+++ estudiantes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define ESTUDIANTES 5
+#define ASIGNATURAS 3
+#define NOTA_APROBATORIA 6.0
+
+int main() {
+    float notas[ESTUDIANTES][ASIGNATURAS];
+
+    printf("Sistema de gestion de calificaciones\n");
+
+    return 0;
+}
